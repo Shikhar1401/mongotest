3129ce5ec386f4b6a560513958f0dfa317cdeea5
shikhar1401 <shikharagarwal0717@gmail.com>
2020-07-12 22:56:18 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,29 @@ import { Http,Response, Headers, RequestOptions } from '@angular/http';
 })
 export class AppComponent {
    
-  constructor(private newService: CommonService) {}
-  Repdata;
-  valButton = 'Save';
+  constructor(private http: Http) {}
 
   ngOnInit(){
-    this.newService.GetUser().subscribe(data => this.Repdata = data);
+   
   }
 
-  onSave = function(user,isValid: boolean) {
-    user.mode = this.valButton;
-    this.newService.saveUser(user)
-    .subscribe(data => {
-      alert(data.data);
-      this.ngOnInit();
-    },
-    error => this.errorMessage = error);
-    }
-  
+  // onSave = function(user,isValid: boolean) {
+  //   user.mode = this.valButton;
+  //   this.newService.saveUser(user)
+  //   .subscribe(data => {
+  //     alert(data.data);
+  //     this.ngOnInit();
+  //   },
+  //   error => this.errorMessage = error);
+  //   }
+  upload(){
+    this.http.get('http://localhost:8000/api/upload').subscribe((res)=>{
+      alert(res);
+    })
+  }
+  download(){
+    this.http.get('http://localhost:8000/api/download').subscribe((res)=>{
+      alert(res);
+    })
+  }
 }
